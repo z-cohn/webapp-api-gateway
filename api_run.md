8e235116c6db6c786bfd85d71c00ed9ac67bd221
Zach Cohn <cohn.zach@gmail.com>
2023-04-16 20:54:49 +0300

    Add basic connection to mongoDB with mongoose

--- api_run.md
+++ api_run.md
@@ -1,9 +1,13 @@
 HOST_PORT=4000
 GUEST_PORT=4000
+DB_USER=zcohn
+DB_PW=arteckMusicDB
 
 docker run \
 -d -i -t \
 -e PORT=$GUEST_PORT \
+-e DB_USER=$DB_USER \
+-e DB_PW=$DB_PW \
 -p $HOST_PORT:$GUEST_PORT \
 --name api \
-zcohn93/webapp-gateway-api:0.6
+zcohn93/webapp-gateway-api:0.7
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const DB_USER = process.env.DB_USER
+const DB_PW = process.env.DB_PW
+const DB_SERVER = 'music-db.azkod92.mongodb.net'
+const dbURI = `mongodb+srv://${DB_USER}:${DB_PW}@${DB_SERVER}/?retryWrites=true&w=majority`
+
+console.log('TEST LOG')
+
+mongoose.connect(dbURI)
+    .then((result) => console.log('CONNECTION SUCCESSFUL'))
+    .catch((err) => console.log('CONNECTION FAILED'))
 
 app.get('/', (req, res) => {
     res.send('Hello World');
